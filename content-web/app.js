9cb6fe226d122b316bde9bbb46816275ea95e570
rcy0228 <rituchandrayadav@gmail.com>
2022-11-25 08:50:24 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("4b53ff81-cc69-45da-861e-716eed03ee60");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
